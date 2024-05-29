f4aa4cad0b442bedae1fdd97fdffac0aa82fed83
miripower <polmivi@gmail.com>
2024-05-29 11:56:08 +0200

    Metodo sin usar .cpp

--- GameClient.cpp/EnemigoFinal.cpp
+++ GameClient.cpp/EnemigoFinal.cpp
@@ -25,17 +25,3 @@ void EnemigoFinal::setFrozenAttack(int pFrozenAttack)
 {
 	frozenAttack = pFrozenAttack;
 }
-
-int EnemigoFinal::realizarAtaque()
-{
-	int randAttack = rand() % 100;
-	if (randAttack >= 0 && randAttack <= 4) {
-		return getFireAttack();
-	}
-	else if (randAttack >= 5 && randAttack <= 10) {
-		return getFrozenAttack();
-	}
-	else {
-		return getAttack();
-	}
-}
